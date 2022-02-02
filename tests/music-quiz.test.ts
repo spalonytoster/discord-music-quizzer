f6eb4d0f13379eb7b7d6e5ea4a39c5f922ede4ce
Maciej Posluszny <m.posluszny@carfax.eu>
2022-02-02 20:20:42 +0100

    Proper mocking of MusicQuiz object in unit tests

--- tests/music-quiz.test.ts
+++ tests/music-quiz.test.ts
@@ -1,14 +1,28 @@
 // const expect = require('chai').expect;
-import {mock} from "ts-mockito";
+import {instance, mock, when} from "ts-mockito";
 import {CommandoMessage} from "discord.js-commando";
 import { MusicQuiz } from "../src/music-quiz";
 import { QuizArgs } from "../src/types/quiz-args";
 import {assert} from "chai";
+import { GuildMember, VoiceChannel, VoiceState } from "discord.js";
 
 describe('MusicQuiz module', () => {
-    const sut: MusicQuiz = new MusicQuiz(mock(CommandoMessage), mock<QuizArgs>());
+    let channel: VoiceChannel = mock(VoiceChannel);
+    let voice: VoiceState = mock(VoiceState);
+    when(voice.channel).thenReturn(channel);
+    let voiceInstance = instance(voice)
+    let member: GuildMember = mock(GuildMember);
+    when(member.voice).thenReturn(voiceInstance);
+    let memberInstance = instance(member);
+    let message: CommandoMessage = mock(CommandoMessage);
+    when(message.member).thenReturn(memberInstance);
+    let messageInstance = instance(message);
 
-    context('Given a list with song names (youtube video names))', () => {
+    let args: QuizArgs = mock<QuizArgs>();
+
+    const sut: MusicQuiz = new MusicQuiz(messageInstance, args);
+
+    context('Given a list with song names (spotify song names))', () => {
         const songList: string[] = [
             "death bed (coffee for your head) (feat. beabadoobee)",
             "Dragostea Din Tei - DJ Ross Radio Remix"
@@ -17,7 +31,8 @@ describe('MusicQuiz module', () => {
         it('Should return strings stripped to only contain song names', () => {
             const result = songList.map(name => sut.stripSongName(name));
 
-            assert(false, 'dupa');
+            assert.equal(result[0], 'death bed');
+            assert.equal(result[1], 'Dragostea Din Tei');
         });
     });
 });
